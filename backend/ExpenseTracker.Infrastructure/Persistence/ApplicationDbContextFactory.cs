517b151271983f14094e277fd537eec2305bd997
SoV3h04ek <mr.ivan121212@gmail.com>
2025-12-09 21:18:58 +0100

    Fix(Infrastructure): Resolve EF Core design-time connection failure by adding IDesignTimeDbContextFactory.

---
+++ backend/ExpenseTracker.Infrastructure/Persistence/ApplicationDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.Runtime.InteropServices;
+
+namespace ExpenseTracker.Infrastructure.Persistence
+{
+    internal class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..\\ExpenseTracker.API"))
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseNpgsql(connectionString, 
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
